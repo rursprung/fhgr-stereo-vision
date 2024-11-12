1f3144ee366a50d33ed242295ee9c4e04c7ce257
Ralph Ursprung <ralph.ursprung@gmail.com>
2024-11-12 20:43:44 +0100

    add image rectification
    also re-format the files as they had the wrong indentation. sorry for
    mixing this in one commit :(
    
    this adds the rectification logic without actually calling it as this
    hasn't been implemented yet. that will come as a follow-up. i've just
    added it here to already show how that'll look like.

--- src/lib.cpp
+++ src/lib.cpp
@@ -3,35 +3,60 @@
 
 namespace stereo_vision {
 
-    auto AnalysisError::ToString() const -> std::string {
-        switch(this->value_) {
-            case kInvalidImage:
-                return "invalid image!";
-            case kNotYetImplemented:
-                return "not yet implemented!";
-            default:
-                throw std::runtime_error("unknown AnalysisError type!");
-        }
+  auto AnalysisError::ToString() const -> std::string {
+    switch (this->value_) {
+      case kInvalidImage:
+        return "invalid image!";
+      case kNotYetImplemented:
+        return "not yet implemented!";
+      default:
+        throw std::runtime_error("unknown AnalysisError type!");
+    }
+  }
+
+  AnalysisError::operator std::string() const {
+    return this->ToString();
+  }
+
+  std::ostream &operator<<(std::ostream &o, AnalysisResult const &analysis_result) {
+    // TODO: add output when adding data
+    return o;
+  }
+
+  StereoVision::StereoVision(Settings const settings) : settings_(std::move(settings)) {
+    cv::Mat R_left, R_right, P_left, P_right, Q;
+
+    cv::stereoRectify(
+        this->settings_.stereo_camera_info.camera_matrix_left, this->settings_.stereo_camera_info.dist_coeffs_left,
+        this->settings_.stereo_camera_info.camera_matrix_right, this->settings_.stereo_camera_info.dist_coeffs_right,
+        this->settings_.stereo_camera_info.image_size, this->settings_.stereo_camera_info.R,
+        this->settings_.stereo_camera_info.T, R_left, R_right, P_left, P_right, Q, cv::CALIB_ZERO_DISPARITY, 0,
+        this->settings_.stereo_camera_info.image_size);
+
+    cv::initUndistortRectifyMap(this->settings_.stereo_camera_info.camera_matrix_left,
+                                this->settings_.stereo_camera_info.dist_coeffs_left, R_left, P_left,
+                                this->settings_.stereo_camera_info.image_size, CV_16SC2,
+                                this->undistort_rectify_map_left.first, this->undistort_rectify_map_left.second);
+    cv::initUndistortRectifyMap(this->settings_.stereo_camera_info.camera_matrix_right,
+                                this->settings_.stereo_camera_info.dist_coeffs_right, R_right, P_right,
+                                this->settings_.stereo_camera_info.image_size, CV_16SC2,
+                                this->undistort_rectify_map_right.first, this->undistort_rectify_map_right.second);
+  }
+
+  auto StereoVision::AnalyzeAndAnnotateImage(cv::Mat const &left_image, cv::Mat const &right_image) const -> std::expected<AnalysisResult, AnalysisError> {
+    if (left_image.data == nullptr || right_image.data == nullptr) {
+      return std::unexpected{AnalysisError::kInvalidImage};
     }
 
-    AnalysisError::operator std::string() const {
-        return this->ToString();
-    }
+    return std::unexpected{AnalysisError::kNotYetImplemented};
+  }
 
-    std::ostream& operator << (std::ostream& o, AnalysisResult const& analysis_result) {
-        // TODO: add output when adding data
-        return o;
-    }
-
-    StereoVision::StereoVision(Settings const settings) : settings_(std::move(settings)) {
-    }
-
-    auto StereoVision::AnalyzeAndAnnotateImage(cv::Mat const& left_image, cv::Mat const& right_image) const -> std::expected<AnalysisResult, AnalysisError> {
-        if (left_image.data == nullptr || right_image.data == nullptr) {
-            return std::unexpected{AnalysisError::kInvalidImage};
-        }
-
-        return std::unexpected{AnalysisError::kNotYetImplemented};
-    }
-
-}
+  auto StereoVision::RectifyImages(cv::Mat const &left_image, cv::Mat const &right_image) const -> std::tuple<cv::Mat, cv::Mat> {
+    cv::Mat left_image_rectified, right_image_rectified;
+    cv::remap(left_image, left_image_rectified, this->undistort_rectify_map_left.first,
+              this->undistort_rectify_map_left.second, cv::INTER_LINEAR);
+    cv::remap(right_image, right_image_rectified, this->undistort_rectify_map_right.first,
+              this->undistort_rectify_map_right.second, cv::INTER_LINEAR);
+    return {left_image_rectified, right_image_rectified};
+  }
+} // namespace stereo_vision
